7d0e6005ad4c0a027f4ac86a73e0fb2ca9e4b463
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-05-30 17:24:48 +0900

    Main6.java

--- 2-3/Main6.java
+++ 2-3/Main6.java
@@ -1,8 +1,8 @@
 public class Main6 {
     public static void main(String[] args) {
-        String price = "1000円";
-        System.out.println("更新前の価格:" + price);
-        price = "1200円";
-        System.out.println("更新後の価格:" + price);
+        int price = 1000;
+        System.out.println("更新前の価格:" + price + "円");
+        int price = 1200;
+        System.out.println("更新後の価格:" + price + "円");
     }
 }
