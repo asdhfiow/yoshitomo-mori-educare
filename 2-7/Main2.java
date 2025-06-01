5fa31368dd81315c43a218afa99b7c5acd201b47
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-06-01 14:50:16 +0900

    Main2.java

--- 2-7/Main2.java
+++ 2-7/Main2.java
@@ -1,7 +1,7 @@
 public class Main2 {
     public static void main(String[] args) {
         int x = 2500;
-        int discount = x * 4 % 5;  //割引額（20%引き）
+        int discount = x * 4 / 5;  //割引額（20%引き）
         int price = 2500 - discount;  //割引後の価格
 
         System.out.println("割引額：" + discount + "円");
