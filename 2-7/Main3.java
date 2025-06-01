4a1c515c795b4611aedaf0de1ae4c30e15a692b2
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-06-01 14:53:44 +0900

    Main3.java

--- 2-7/Main3.java
+++ 2-7/Main3.java
@@ -2,7 +2,7 @@ public class Main3 {
     public static void main(String[] args) {
         int attack = 15;
         int defense = 5;
-        int damage = (attack * 2) - defense / 3;//小数点以下は切り捨て
+        int damage = (attack * 2 - defense) / 3;//小数点以下は切り捨て
         
         System.out.println("与えるダメージ：" + damage);
     }
