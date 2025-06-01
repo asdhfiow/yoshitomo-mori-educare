01612a422c938de5d0a11ecc8e63fc12ddf51d97
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-06-01 15:59:11 +0900

    Main8.java

--- 2-7/Main8.java
+++ 2-7/Main8.java
@@ -1,15 +1,20 @@
+import java.util.Scanner;
+
 public class Main8 {
     public static void main(String[] args) {
-        int attack = 50;
-        int defense = 20;
-        int damage = attack - defense;
-        double critical = damage * 1.5;
-        int criticalDamage = (int) critical;
-        
-        System.out.println("攻撃力を入力してください：" + attack);
-        System.out.println("防御力を入力してください：" + defense);
-        System.out.println("通常ダメージ：" + damage);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("攻撃力を入力してください：");
+        int attack = Integer.parseInt(scanner.nextLine());
+        System.out.print("防御力を入力してください：");
+        int defense = Integer.parseInt(scanner.nextLine());
+
+        int normalDamage = attack - defense;
+        int criticalDamage = (int)(normalDamage * 1.5);
+
+        System.out.println("通常ダメージ：" + normalDamage);
         System.out.println("クリティカルダメージ：" + criticalDamage);
 
+        scanner.close();
     }
 }
