861720b3d70a285b49d383d305d3adb3e6628f8a
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-06-01 15:57:58 +0900

    Main6.java

--- 2-7/Main6.java
+++ 2-7/Main6.java
@@ -1,10 +1,18 @@
-public class Main6 {
-    public static void main(String[] args) {
-        String exPoint = "1000";
-        int ExPoint = Integer.parseInt(exPoint); //小数点以下切り捨て
-        int bonusPoint = ExPoint * 125 / 100;
+import java.util.Scanner;
 
-        System.out.println("現在の経験値を入力してください：" + exPoint);
-        System.out.println("ボーナス後の経験値：" + bonusPoint);
+public class Main6{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("現在の経験値を入力してください：");
+        String input = scanner.nextLine();
+
+        int currentExp = Integer.parseInt(input);
+        int bonusExp = (int)(currentExp * 0.25);
+        int totalExp = currentExp + bonusExp;
+
+        System.out.println("ボーナス後の経験値：" + totalExp);
+
+        scanner.close();
     }
 }
