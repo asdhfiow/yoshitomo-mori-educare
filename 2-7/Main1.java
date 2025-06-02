92445cfbe0541841dc6af95ab60cf02864cf68e8
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-06-02 17:11:48 +0900

    Main1.java

--- 2-7/Main1.java
+++ 2-7/Main1.java
@@ -1,9 +1,9 @@
 public class Main1{
     public static void main(String[] args) {
-        int x = 480;
-        int y = 160;
-        int z = 130;
-        int sum = x + y + z;
+        int bento = 480;
+        int tea = 160;
+        int onigiri = 130;
+        int sum =  bento + tea + onigiri;
         
         System.out.println("合計金額は: " + sum + "円です。");
     }
