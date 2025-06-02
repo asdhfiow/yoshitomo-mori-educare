c06a0ff043cec01cd9df31a208de8c1c801c5363
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-06-02 17:09:24 +0900

    Main7.java

--- 2-7/Main7.java
+++ 2-7/Main7.java
@@ -1,11 +1,8 @@
 public class Main7 {
     public static void main(String[] args) {
-        int live = 100;
-        live --;
-        System.out.println("攻撃1回目：残り体力" + live);
-        live --;
-        System.out.println("攻撃2回目：残り体力" + live);
-        live --;
-        System.out.println("攻撃3回目：残り体力" + live);
+        int health = 100;
+        System.out.println("攻撃1回目：残り体力" + health--);
+        System.out.println("攻撃2回目：残り体力" + health--);
+        System.out.println("攻撃3回目：残り体力" + health--);
     }
 }
