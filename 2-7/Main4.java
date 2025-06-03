72ba0e268283b1155e08b37809b070b2a28c9d26
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-06-03 10:49:58 +0900

    Main4.java

--- 2-7/Main4.java
+++ 2-7/Main4.java
@@ -1,7 +1,8 @@
 public class Main4 {
     public static void main(String[] args) {
-        double maxHP = 1000.0 * 83.7 / 100; //最大HP（小数点以下は切り捨て）
-        int currentHP = (int)maxHP;//小数点以下切り捨て
+        double maxHP = 1000.0;
+        double damaged = maxHP * 83.7 / 100; 
+        int currentHP = (int)damaged;
 
         System.out.println("最大HP：" + currentHP);
     }
