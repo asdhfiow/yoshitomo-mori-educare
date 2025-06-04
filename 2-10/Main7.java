754e6f4de73b1200fba94a5621f19e63aad85637
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-06-04 12:43:21 +0900

    Main7.java

--- 2-10/Main7.java
+++ 2-10/Main7.java
@@ -1,30 +1,39 @@
 public class Main7 {
-    public static void main(String[] args) {
-        int key = 2;
-        int pressCount = 2;
-
-        String combo = key + "-" + pressCount;
-        switch (combo){
-            case "1-1":{
-                System.out.println("A");
-            }break;
-            case "1-2":{
-                System.out.println("B");
-            }break;
-            default: {
-                System.out.println("C");
+        public static void main(String[] args) {
+            int key = 2;
+            int pressCount = 2;
+    
+            switch (key) {
+                case 1:
+                    switch (pressCount) {
+                        case 1:
+                            System.out.println("A");
+                            break;
+                        case 2:
+                            System.out.println("B");
+                            break;
+                        default:
+                            System.out.println("C"); // key1でその他の押下回数
+                    }
+                    break;
+    
+                case 2:
+                    switch (pressCount) {
+                        case 1:
+                            System.out.println("D");
+                            break;
+                        case 2:
+                            System.out.println("E");
+                            break;
+                        default:
+                            System.out.println("F"); // key2でその他の押下回数
+                    }
+                    break;
+    
+                default:
+                    System.out.println("invalid key"); // key1・key2以外
             }
-      switch(combo){
-            case "2-1":{
-                System.out.println("D");
-            }break;
-            case "2-2":{
-                System.out.println("E");
-            }break;
-        default:{
-            System.out.println("F");
-        }
         }
     }
-}
-}
+    
+    
