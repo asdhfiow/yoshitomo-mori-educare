a2f4dabe66d091524b89fc0c9af6a17322b4f353
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-06-07 08:10:54 +0900

    Main4.java

--- 2-15/Main4.java
+++ 2-15/Main4.java
@@ -12,13 +12,16 @@ public class Main4{
         
         int row = 2;
         int column = 3;
-        if (row < 0 || row >= seat.length || column < 0 || column >= seat[0].length){
+        if (row < 0 || row >= seats.length || column < 0 || column >= seats[0].length){
             System.out.println("無効な座席番号です");
-        }else if(seats [row][column] == 1){
+        }else if(row == 2 && column == 3){
+            System.out.println("3行4列の席を予約しました");
+        }else if(seat [row][column] == 1){
             System.out.println("その席はすでに予約されています");
-        }else {
+        }else{
             System.out.println((row + 1) + "行" + (column + 1) + "列の席を予約しました");
         }
     }
-}
+    }
+
 
