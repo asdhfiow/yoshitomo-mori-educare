23734049f74598eb64f74a2238c566644bfe2c94
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-06-07 18:01:30 +0900

    Main4.java

--- 2-15/Main4.java
+++ 2-15/Main4.java
@@ -1,3 +1,4 @@
+
 public class Main4{
     public static void main(String[] args){
         int [][] seats = new int[5][6];
@@ -9,7 +10,14 @@ public class Main4{
             {1,0,0,0,0,1},
             {0,0,1,0,0,0}
         };
-        
+        System.out.println("現在の座席状況：");
+        for (int i = 0; i < seat.length; i++){
+            for (int j = 0; j < seat[i].length; j++){
+                System.out.print( "[" +  seat[i][j] + "]");
+            }
+            System.out.println();
+
+        }
         int row = 2;
         int column = 3;
         if (row < 0 || row >= seats.length || column < 0 || column >= seats[0].length){
