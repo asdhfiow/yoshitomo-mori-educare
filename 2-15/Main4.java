2abe3866d5792020ca2abe5825afd343d08d3324
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-06-08 17:30:47 +0900

    Main4.java

--- 2-15/Main4.java
+++ 2-15/Main4.java
@@ -1,35 +1,32 @@
-
-public class Main4{
-    public static void main(String[] args){
-        int [][] seats = new int[5][6];
-
-        int[][] seat= {
-            {0,0,1,0,0,0},
-            {0,1,0,0,1,0},
-            {0,0,0,0,0,0},
-            {1,0,0,0,0,1},
-            {0,0,1,0,0,0}
+public class Main4 {
+    public static void main(String[] args) {
+        int[][] seats = {
+            {0, 0, 1, 0, 0, 0}, 
+            {0, 1, 0, 0, 1, 0}, 
+            {0, 0, 0, 0, 0, 0}, 
+            {1, 0, 0, 0, 0, 1}, 
+            {0, 0, 1, 0, 0, 0}  
         };
-        System.out.println("現在の座席状況：");
-        for (int i = 0; i < seat.length; i++){
-            for (int j = 0; j < seat[i].length; j++){
-                System.out.print( "[" +  seat[i][j] + "]");
+
+        int row = 2;   
+        int column = 3; 
+
+        System.out.println("現在の座席状況:");
+        for (int i = 0; i < seats.length; i++) {
+            for (int j = 0; j < seats[i].length; j++) {
+                System.out.print("[" + seats[i][j] + "]");
             }
             System.out.println();
-
         }
-        int row = 2;
-        int column = 3;
-        if (row < 0 || row >= seats.length || column < 0 || column >= seats[0].length){
+
+
+                if (row < 0 || row >= seats.length || column < 0 || column >= seats[0].length) {
             System.out.println("無効な座席番号です");
-        }else if(seat [row][column] == 1){
-            System.out.println("その席はすでに予約されています");
-        }else if(row == 2 && column == 3){
-            System.out.println("3行4列の席を予約しました");
-        }else{
-            System.out.println((row + 1) + "行" + (column + 1) + "列の席を予約しました");
+        } else if (seats[row][column] == 1) {
+            System.out.println("指定された座席はすでに予約済みです");
+        } else {
+            seats[row][column] = 1; 
+            System.out.println((row + 1) + "行" + (column + 1) + "列の座席を予約しました");
         }
     }
-    }
-
-
+}
