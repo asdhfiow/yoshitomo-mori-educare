35c26d9814936fa4ac98f4c47f7d79d21cbb28a4
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-06-08 17:31:11 +0900

    Main3.java

--- 2-15/Main3.java
+++ 2-15/Main3.java
@@ -1,19 +1,19 @@
 public class Main3 {
     public static void main(String[] args) {
-        double[] temparatures = {25.5, 26.0, 24.5, 26.5, 27.0, 25.0, 24.0};
+        double[] temperatures = {25.5, 26.0, 24.5, 26.5, 27.0, 25.0, 24.0};
         String[] day = {"月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日", "日曜日"};
-        double totalTemparature = 0;
+        double totalTemperature = 0;
 
-        for (double temparature: temparatures){
+        for (double temparature: temperatures){
             {
-                totalTemparature += temparature;
+                totalTemperature += temparature;
             }
     }
         for(int i = 0; i < day.length; i++){
-           System.out.println(day[i] + "の気温：" + temparatures[i] + "度");
+           System.out.println(day[i] + "の気温：" + temperatures[i] + "度");
         }
      
-    System.out.println("平均気温：" + totalTemparature / 7);
+    System.out.println("平均気温：" + totalTemperature / temperatures.length);
 
 }
 }
