97ab9a84ddbab5410fbe3cdbb64da58aaebf2dbb
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-06-11 18:33:17 +0900

    Main1.java

--- 2-18/Main1.java
+++ 2-18/Main1.java
@@ -2,12 +2,13 @@ public class Main1{
     public static void main(String[] args){
         int price = 1000;
         int quantity = 3; 
-        calculate(price,quantity);
+        calculateTotal(price,quantity);
     }
     
-    public static double calculate(int price, int quantity){
-        double result = (price * quantity) * 1.08;
+    public static double calculateTotal(int price, int quantity){
         System.out.println("商品価格:" + price + "円," + "数量：" + quantity);
+        double result = (price * quantity) * 1.08;
+
         System.out.println("合計金額（税込）：" + (int)result + "円");
         return result;
     }
