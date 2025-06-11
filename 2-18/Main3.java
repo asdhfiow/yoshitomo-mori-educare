dd04d8b9cdd64b78a110ab9fab7284fa7c1b3e0f
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-06-11 18:33:56 +0900

    Main3.java

--- 2-18/Main3.java
+++ 2-18/Main3.java
@@ -2,11 +2,11 @@ public class Main3 {
     public static void main(String[] args){
         String input ="Hello";
         String reversed = reverse(input);
-        System.out.println(input);
-        System.out.println(reversed);
+        System.out.println( "入力：" + "\"" + input + "\"");
+        System.out.println("出力：" + "\"" + reversed + "\"");
     }   
     public static String reverse(String input){
-        String reversed = new StringBuilder("Hello").reverse().toString();
+        String reversed = new StringBuilder(input).reverse().toString();
 
         return reversed;
     }
