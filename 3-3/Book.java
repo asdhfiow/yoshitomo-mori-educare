8c2a91fd4f1a94b30b8c4545755434ccedb4b10e
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-06-19 17:23:29 +0900

    Book.java

--- 3-3/Book.java
+++ 3-3/Book.java
@@ -1,27 +1,33 @@
 public class Book {
-    //フィールド
-    String title;
-    boolean isLent = false;//最初は貸出中ではない
+    // フィールド
+    private String title;
+    private boolean isLent; // 初期値falseなので代入不要
 
-void setTitle(String title){
-    this.title = title; // 引数のtitleをフィールドに設定
-}
-void getTitle(){
-    System.out.println(this.title + "を登録しました");
-}
+    // タイトルを設定するメソッド
+    public void setTitle(String title){
+        this.title = title;
+    }
 
-void lend(){
-    if (!isLent){
-        isLent = true;
-        System.out.println("本を貸し出しました");
+    // タイトルを取得するメソッド（出力ではなく return）
+    public String getTitle(){
+        return this.title;
+    }
+
+    // 本を貸し出す処理
+    public void lend(){
+        if (!isLent){
+            isLent = true;
+            System.out.println("本を貸し出しました");
         } else {
             System.out.println("申し訳ありません。この本は貸し出中です");
         }
-}
-void returnBook(){
-    if(isLent){
-        isLent = false;
-        System.out.println("本が返却されました");
     }
-}
+
+    // 本を返却する処理
+    public void returnBook(){
+        if (isLent){
+            isLent = false;
+            System.out.println("本が返却されました");
+        }
+    }
 }
