1315af9faa789883b667d2c2815044763a26b5d1
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-06-19 17:24:06 +0900

    Main2.java

--- 3-3/Main2.java
+++ 3-3/Main2.java
@@ -1,21 +1,20 @@
 public class Main2 {
     public static void main(String[] args){
 
-        Book book = new Book(); //インスタンスの生成
+        Book book = new Book(); // インスタンスの生成
         
-        //java入門を登録しました　と出力
-        book.setTitle("java入門"); //タイトルの設定
-        book.getTitle(); //タイトルの取得
+        // タイトルを設定して出力
+        book.setTitle("Java入門");
+        System.out.println(book.getTitle() + "を登録しました");
 
-
-        //2回貸し出そうとする処理
+        // 2回貸し出そうとする処理
         book.lend();
         book.lend();
 
-        //本を返却する処理
+        // 本を返却する処理
         book.returnBook();
 
-        //再度貸し出す処理
+        // 再度貸し出す処理
         book.lend();
     }
 }
