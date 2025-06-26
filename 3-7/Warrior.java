b058b3d6866875dc971b51ef8216304f5f375c6c
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-06-26 14:39:28 +0900

    Update Warrior.java

--- 3-7/Warrior.java
+++ 3-7/Warrior.java
@@ -1,12 +1,1 @@
-class Warrior extends Character{
-    public String job = "戦士";
 
-    Warrior(String name, int hp){
-        super(name, hp);
-    }
-    @Override
-    public void showStatus(){
-        super.showStatus();
-        System.out.println("職業：" + job);
-    }
-}
