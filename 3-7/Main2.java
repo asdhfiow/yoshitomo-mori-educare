52f1735666938dbe0367dbbf649858315e9cf600
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-06-26 14:38:52 +0900

    Update Main2.java

--- 3-7/Main2.java
+++ 3-7/Main2.java
@@ -1,8 +1,1 @@
-public class Main2 {
-    public static void main(String[] arts){
-        Monster m = new Monster();
-        m.attack();
-        Slime s = new Slime();
-        s.attack();
-    }
-}
+
