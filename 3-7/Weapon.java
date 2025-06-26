694849d87d89bf855d3f6c48e901510bdf430300
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-06-26 14:39:34 +0900

    Update Weapon.java

--- 3-7/Weapon.java
+++ 3-7/Weapon.java
@@ -1,15 +1,1 @@
-class Weapon {
-    String name;
-    int power;
 
-    public Weapon(String name, int power) {
-        this.name = name;
-        this.power = power;
-    }
-    public String getName(){
-        return name;
-    }
-    public int getPower(){
-        return power;
-    }
-    }
