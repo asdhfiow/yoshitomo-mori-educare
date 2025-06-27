724760bc27e2c966c9f0028f97fd57effe111cbd
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-06-27 13:29:11 +0900

    SavingAccount.java

--- 3-11/SavingAccount.java
+++ 3-11/SavingAccount.java
@@ -1,6 +1,36 @@
-public interface SavingAccount {
-    void deposit(int amount);
-    void withdraw(int amount);
-    void displayAccountInfo();
-    int getBalance();
+public class SavingAccount implements BankAccount {
+    String bankAccountName;
+    int balance;
+    String history = "";
+
+    public SavingAccount(String bankAccountName, int balance) {
+        this.bankAccountName = bankAccountName;
+        this.balance = balance;
+        history += "口座作成: 初期残高 = " + balance + "\n";
+    }
+
+    @Override
+    public void deposit(int amount) {
+        balance += amount;
+        history += "入金: " + amount + "\n";
+    }
+
+    @Override
+    public void withdraw(int amount) {
+        balance -= amount;
+        history += "出金: " + amount + "\n";
+    }
+
+    @Override
+    public void displayAccountInfo() {
+        System.out.println("口座名義人: " + bankAccountName);
+        System.out.println("残高: " + balance);
+        System.out.println("取引履歴:");
+        System.out.print(history);
+    }
+
+    @Override
+    public int getBalance() {
+        return balance;
+    }
 }
