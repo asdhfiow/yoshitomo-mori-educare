fc1bc48e5f0e7b39b451c1fce825eb1ed4f5e9d2
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-06-27 13:28:54 +0900

    Main5.java

--- 3-11/Main5.java
+++ 3-11/Main5.java
@@ -1,9 +1,8 @@
 public class Main5 {
     public static void main(String[] args) {
-        SavingAccount sa = new BankAccount("山田太郎", 10000);
+        BankAccount sa = new SavingAccount("山田太郎", 10000);  // ← 多態性
         sa.deposit(5000);
         sa.withdraw(3000);
         sa.displayAccountInfo();
-
     }
 }
