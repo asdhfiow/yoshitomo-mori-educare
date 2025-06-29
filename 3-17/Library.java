0af21eb04f16ba31f8c02596f0fc010bf27bafc1
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-06-29 20:39:09 +0900

    Library.java

--- 3-17/Library.java
+++ 3-17/Library.java
@@ -1,24 +1,20 @@
-public class Library extends Book {
-
-    public Library(String title, boolean isLent) {
-        super(title, isLent);
-    }
+public class Library {
 
     // 本を貸し出す
-    public void lendBook() {
-        if (!isLent()) {
-            setLent();
-            System.out.println("「" + getTitle() + "」を貸し出しました");
+    public void lendBook(Book book) {
+        if (!book.isLent()) {
+            book.setLent();
+            System.out.println("「" + book.getTitle() + "」を貸し出しました");
         } else {
             System.out.println("エラー：この本は既に貸し出し中です");
         }
     }
 
     // 本を返却する
-    public void returnBook() {
-        if (isLent()) {
-            setReturned();
-            System.out.println("「" + getTitle() + "」が返却されました");
+    public void returnBook(Book book) {
+        if (book.isLent()) {
+            book.setReturned();
+            System.out.println("「" + book.getTitle() + "」が返却されました");
         } else {
             System.out.println("エラー：この本は貸し出されていません");
         }
