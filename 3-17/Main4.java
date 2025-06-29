c4bc169887417effc1cc7a11619fccc4c9a62aca
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-06-29 15:26:16 +0900

    Main4.java

--- 3-17/Main4.java
+++ 3-17/Main4.java
@@ -1,10 +1,11 @@
 public class Main4 {
     public static void main(String[] args) {
-        Library lib = new Library("Java入門", false);
+        Library lib = new Library();
+        Book book = new Book("Java入門", false);
 
-        lib.lendBook();    
-        lib.lendBook();    
-        lib.returnBook();  
-        lib.lendBook();    
+        lib.lendBook(book);    
+        lib.lendBook(book);    
+        lib.returnBook(book);  
+        lib.lendBook(book);    
     }
 }
