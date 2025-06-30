9a3732202e045ac2c50517309c89ceda0100c691
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-06-30 13:19:33 +0900

    Main3.java

--- 3-17/Main3.java
+++ 3-17/Main3.java
@@ -1,18 +1,17 @@
 public class Main3 {
     public static void main(String[] args) {
         Product pr = new Product("りんご", 100);
-        pr.getProduct();
-        pr.getQuantities();
+
+        System.out.println("商品名: " + pr.getName());
+        System.out.println("在庫数: " + pr.getStock() + "個");
         pr.removeStock(50);
 
-        pr.getProduct();
-        pr.getQuantities();
+        System.out.println("商品名: " + pr.getName());
+        System.out.println("在庫数: " + pr.getStock() + "個");
         pr.addStock(10);
 
-        pr.getProduct();
-        pr.getQuantities();
-        pr.removeStock(999999);
-
-        
+        System.out.println("商品名: " + pr.getName());
+        System.out.println("在庫数: " + pr.getStock() + "個");
+        pr.removeStock(44444444); // 在庫不足エラー出るはず
     }
 }
