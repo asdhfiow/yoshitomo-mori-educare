c0e7ac1971d110b08443d287695016a75e7ddbb1
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-07-01 18:15:06 +0900

    Library.java

--- 3-19/Library.java
+++ 3-19/Library.java
@@ -1,23 +1,23 @@
 public class Library {
     static int availableBooks = 100; // 貸し出し可能な本の総数
 
-    public void display(){
+    static void display(){
         System.out.println("貸出可能な本：" + availableBooks);
     }
-    public void borrowBooks(){
-        if(availableBooks < 0){
-            System.out.println("貸出できる本がありません");
-        }else{
-            System.out.println("一冊借りました");
-            availableBooks --;
-        }
+    static void borrowBooks(){
+    if(availableBooks > 0){
+        System.out.println("１冊本を貸りました");
+        availableBooks --;
+    }else{
+        System.out.println("貸出可能な本はありません");
     }
+}
     // 本を返却
-    public void returnBooks(){
+    static void returnBooks(){
         availableBooks ++;
         System.out.println("本を返却しました");
     }
-    public static int getAvailableBooks(){
+    static int getAvailableBooks(){
         return availableBooks;
     }
 }
