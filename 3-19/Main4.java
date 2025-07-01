bd2959108412687076cc71daca609357f81673e1
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-07-01 18:15:52 +0900

    Main4.java

--- 3-19/Main4.java
+++ 3-19/Main4.java
@@ -1,16 +1,13 @@
 public class Main4 {
     public static void main(String[] args) {
-        ExchangeRate er = new ExchangeRate();
+        int jpy1 = ExchangeRate.toJPY("USD", 100.0);
+        System.out.println("100ドル = " + jpy1 + "円");
 
-        double yen1 = er.toJPY("USD", 100.0);
-        System.out.println("100ドル = " + (int)yen1 + "円");        
+        ExchangeRate.updateRates(145.0, 160.0);
 
-        er.updateRate(150.0, 160.0);
-
-        double yen2 = er.toJPY("USD", 100.0);
-        System.out.println("100ドル = " + (int)yen2 + "円");
-
-        double yen3 = er.toJPY("EUR", 80.0);
-        System.out.println("80ユーロ = " + (int)yen3 + "円");
+        int jpy2 = ExchangeRate.toJPY("USD", 100.0);
+        System.out.println("100ドル = " + jpy2 + "円");
+        int jpy3 = ExchangeRate.toJPY("EUR", 80.0);
+        System.out.println("80ユーロ = " + jpy3 + "円");
     }
 }
