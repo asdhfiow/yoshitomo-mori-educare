22a622da1008240ba5eb3fd3da1c491b9592d83d
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-07-11 19:12:23 +0900

    Calculator.java

--- 4-9/Calculator.java
+++ 4-9/Calculator.java
@@ -1,5 +1,8 @@
 public class Calculator{
     public int divide(int a, int b)throws ArithmeticException{
+        if(b == 0){
+            throw new ArithmeticException();
+        }
         return a/b;
     }
 }
