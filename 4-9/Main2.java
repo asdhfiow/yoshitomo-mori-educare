44c286953dface7d0aadd51d46963b9c055b1714
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-07-11 19:13:27 +0900

    Main2.java

--- 4-9/Main2.java
+++ 4-9/Main2.java
@@ -1,19 +1,20 @@
-import java.util.Arrays;
-
 public class Main2 {
     public static void main(String[] args) {
         int[] numbers = {10, 20, 30};
         ArrayAccessor accessor = new ArrayAccessor(numbers);
 
-        int[] testIndexes = {1, 5};  // 位置1と位置5でテスト
+        System.out.println("\n位置1の要素");
+        try {
+            System.out.println(accessor.getElement(1));
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("エラー：指定された位置は範囲外です");
+        }
 
-        for (int index : testIndexes) {
-            System.out.println("\n位置" + index + "の要素");
-            try {
-                System.out.println(accessor.getElement(index));
-            } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println("エラー：指定された位置は範囲外です");
-            }
+        System.out.println("\n位置5の要素");
+        try {
+            System.out.println(accessor.getElement(5));
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("エラー：指定された位置は範囲外です");
+        }
     }
-}
 }
