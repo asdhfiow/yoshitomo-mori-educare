baef5042cb5ad8552114c5bf2e83496630d20962
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-07-12 14:54:32 +0900

    Main1.java

--- 4-9/Main1.java
+++ 4-9/Main1.java
@@ -7,8 +7,6 @@ public class Main1 {
             System.out.println("計算結果：" + result);
         } catch(ArithmeticException e){
             System.out.println("10 ÷ 0 = エラー：0で割ることはできません");
-        }finally{
-            int normalResult = calc.divide(10, 5);
         }
     }
 }
