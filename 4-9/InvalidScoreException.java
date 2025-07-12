4369ed1af9f610b3276e7342cecb129884dcc4e3
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-07-12 20:49:42 +0900

    InvalidScoreException.java

--- 4-9/InvalidScoreException.java
+++ 4-9/InvalidScoreException.java
@@ -1,6 +1,6 @@
 public class InvalidScoreException extends GameScoreException {
     // TODO: コンストラクタを実装
    public InvalidScoreException(int score){
-    super(score, "エラー: Score cannot be negative");
+    super(score, "Score cannot be negative");
    }
 }
