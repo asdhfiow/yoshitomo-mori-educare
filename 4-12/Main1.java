706a82b02541d08f1d50930b2b25855191d90d3a
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-07-14 17:45:09 +0900

    Main1.java

--- 4-12/Main1.java
+++ 4-12/Main1.java
@@ -3,7 +3,8 @@ public class Main1 {
         GameItem gi1 = new GameItem("回復薬", 100);
         GameItem gi2 = new GameItem("魔法の杖", 150);
 
-        System.out.println("アイテム情報： \n" + gi1);
+        System.out.println("アイテム情報：");
+        System.out.println(gi1);
         System.out.println(gi2);
     }
 }
