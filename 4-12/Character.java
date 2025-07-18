d7d88a38fbfe32983669622775f0093e9a78c25f
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-07-18 10:50:00 +0900

    Character.java

--- 4-12/Character.java
+++ 4-12/Character.java
@@ -1,25 +1,27 @@
-public class Character implements Comparable <Character>{
-   private int hp;
-   private int attack;
-   private String name;
+public class Character implements Comparable<Character> {
+    private int hp;
+    private int attack;
+    private String name;
 
-    public Character(String name, int hp, int attack){
+    public Character(String name, int hp, int attack) {
         this.name = name;
         this.hp = hp;
         this.attack = attack;
     }
-    public int getBattlePower(){
+
+    public int getBattlePower() {
         return hp + attack;
     }
-    // 戦闘力を後順でソート
+
+    // 戦闘力を降順でソート
     @Override
-    public int compareTo(Character other){
-        return Integer.compare(other.getBattlePower(), - this.getBattlePower());
+    public int compareTo(Character other) {
+        return Integer.compare(other.getBattlePower(), this.getBattlePower());
     }
-    
+
     // キャラクター情報を文字列で返す
     @Override
-    public String toString(){
-        return name + "(HP：" + hp + "攻撃力：" + attack + ")" + "戦闘力：" + getBattlePower();
+    public String toString() {
+        return name + " (HP:" + hp + " 攻撃力:" + attack + ") 戦闘力:" + getBattlePower();
     }
 }
