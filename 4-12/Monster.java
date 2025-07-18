b5f61571dc45f7e49e1919a7b78a014c2a81aa03
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-07-18 10:49:12 +0900

    Monster.java

--- 4-12/Monster.java
+++ 4-12/Monster.java
@@ -1,16 +1,37 @@
-public class Monster {
-    String name;
-    int level;
+import java.util.Objects;
 
-    public Monster(String name, int level){
+public class Monster {
+    private String name;
+    private int level;
+
+    public Monster(String name, int level) {
         this.name = name;
         this.level = level;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
     @Override
-    public boolean equals(Object obj){
-        if(obj == this) return true;
-        if(!(obj instanceof Monster)) return false;
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Monster)) return false;
         Monster other = (Monster) obj;
         return this.name.equals(other.name) && this.level == other.level;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, level);
+    }
+
+    @Override
+    public String toString() {
+        return name + "Lv." + level;
     }
 }
