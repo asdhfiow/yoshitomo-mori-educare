9472845ce34807fb8a86491f8afff3747cb35221
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-07-18 10:49:32 +0900

    Main2.java

--- 4-12/Main2.java
+++ 4-12/Main2.java
@@ -5,17 +5,17 @@ public class Main2 {
         Monster monster3 = new Monster("スライム", 10);
 
         // monster1とmonster2の比較
-        if(monster1.equals(monster2)) {
-            System.out.println(monster1.name + "Lv." + monster1.level + " と " + monster2.name + "Lv." + monster2.level + " は同じモンスターです");
+        if (monster1.equals(monster2)) {
+            System.out.println(monster1 + " と " + monster2 + " は同じモンスターです");
         } else {
-            System.out.println(monster1.name + "Lv." + monster1.level + " と " + monster2.name + "Lv." + monster2.level + " は違うモンスターです");
+            System.out.println(monster1 + " と " + monster2 + " は違うモンスターです");
         }
 
         // monster1とmonster3の比較
-        if(monster1.equals(monster3)) {
-            System.out.println(monster1.name + "Lv." + monster1.level + " と " + monster3.name + "Lv." + monster3.level + " は同じモンスターです");
+        if (monster1.equals(monster3)) {
+            System.out.println(monster1 + " と " + monster3 + " は同じモンスターです");
         } else {
-            System.out.println(monster1.name + "Lv." + monster1.level + " と " + monster3.name + "Lv." + monster3.level + " は違うモンスターです");
+            System.out.println(monster1 + " と " + monster3 + " は違うモンスターです");
         }
     }
 }
