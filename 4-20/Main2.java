b1315578e7820de45bfc425e3d577ff86a41976c
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-08-04 10:21:07 +0900

    Main2.java

--- 4-20/Main2.java
+++ 4-20/Main2.java
@@ -1,26 +1,17 @@
 import java.io.*;
 
 public class Main2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         // ここにコードを書いてください
-            FileReader reader = null;
 
-        try {
-            reader = new FileReader("exercise.txt");
+
+        try (FileReader reader = new FileReader("exercise.txt")){
             int data;
-            while((data = reader.read()) != 1){
+            while((data = reader.read()) != -1){
                 System.out.print((char) data);
         }
         } catch (IOException e){
             System.out.print("ファイルの読み込み中にエラーが発生しました。");
-        } finally {
-            if (reader != null){
-                try {
-                    reader.close();
-                } catch (IOException e){
-                    System.out.println("ファイルを閉じる際にエラーが発生しました。");
-                }
-            }
-        }
+        } 
     }
 }
