824c14673561bc6caf7cbd0a3a06a9acdccea51a
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-08-22 12:13:17 +0900

    SearchProduct.java

--- 5-14/SearchProduct.java
+++ 5-14/SearchProduct.java
@@ -6,52 +6,39 @@ import java.sql.SQLException;
 
 public class SearchProduct {
     public static void main(String[] args) {
-        String url = "jdbc:postgresql://localhost:5432/educure_db"; // データベースURL
-        String user = "yoshitomo"; // データベースのユーザー名
-        String password = "Yoshitomo12"; // データベースのパスワード
+        String url = "jdbc:postgresql://localhost:5432/educure_db";
+        String user = "yoshitomo";
+        String password = "Yoshitomo12";
         
         String query = "SELECT product_name, price FROM products WHERE price >= ?";
-        // ここにコードを書いてください
-
 
         try {
-            // ドライバを使えるようにする
             Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
             throw new IllegalStateException("ドライバのロードに失敗しました。");
         }
 
-
-        Connection con = null;
-
-
-        try {
-            con = DriverManager.getConnection(url,user,password);
-            PreparedStatement pstmt = con.prepareStatement(query);
-
+        
+        try (
+            Connection con = DriverManager.getConnection(url, user, password);
+            PreparedStatement pstmt = con.prepareStatement(query)
+        ) {
             pstmt.setInt(1, 100000);
 
-            ResultSet rs = pstmt.executeQuery();
-
-            while (rs.next()){
-                String product_name = rs.getString("product_name");
-                int price = rs.getInt("price");
-
-                System.out.println("商品名：" + product_name + "価格：" + price);
-            }
-
-            rs.close();
-            pstmt.close();
-        }catch(SQLException e){ 
-            e.printStackTrace();
-        } finally {
-            if (con != null) {
-                try {
-                    con.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()){
+                    String product_name = rs.getString("product_name");
+                    int price = rs.getInt("price");
+                    
+                    // 出力
+                    System.out.print("商品名: " + product_name + ", 価格: " + price + " ");
                 }
+                // 最後に改行する
+                System.out.println();
             }
+
+        } catch (SQLException e) { 
+            e.printStackTrace();
         }
    }
 }
