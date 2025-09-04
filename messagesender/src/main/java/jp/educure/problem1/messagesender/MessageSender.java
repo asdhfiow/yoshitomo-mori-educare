a070bacca3ba129c50c91379570b194dc8857f81
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-09-04 20:50:12 +0900

    Add files via upload

---
+++ messagesender/src/main/java/jp/educure/problem1/messagesender/MessageSender.java
@@ -0,0 +1,5 @@
+package jp.educure.problem1.messagesender;
+
+public interface MessageSender {
+    void sendMessage(String message);
+}
---
+++ messagesender/src/test/java/jp/educure/problem1/messagesender/MessagesenderApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.educure.problem1.messagesender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagesenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
