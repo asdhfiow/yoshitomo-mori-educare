d1dfbce9089a48b4a94ebe99dd5923dcddf0b4e2
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-09-04 20:48:36 +0900

    Add files via upload

---
+++ greetingservice/src/main/java/jp/educure/problem2/greetingservice/GreetingController.java
@@ -0,0 +1,20 @@
+package jp.educure.problem2.greetingservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private final GreetingService greetingService;
+
+    // コンストラクタインジェクション
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    @GetMapping("/greet")
+    public String greet() {
+        return greetingService.greet();
+    }
+}
