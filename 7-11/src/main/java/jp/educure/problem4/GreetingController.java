0e2e681f1716e0060d483a5483936f8a9291ed9a
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-09-08 14:56:07 +0900

    GreetingController.java

--- 7-11/src/main/java/jp/educure/problem4/GreetingController.java
+++ 7-11/src/main/java/jp/educure/problem4/GreetingController.java
@@ -10,13 +10,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class GreetingController {
 
     // 初期画面: フォームを表示
-    @GetMapping("/")
+    @GetMapping("/greeting")
     public String showForm() {
         return "greetingForm"; // greetingForm.htmlを表示
     }
 
     // フォーム送信後の処理（POST）
-    @PostMapping("/greet")
+    @PostMapping("/greeting")
     public String greet(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "greetingResult"; // greetingResult.htmlを表示
