8bf5f97e20edaca411bc7ceb3c71fefedeba4bcf
yoshitomo777777 <76730709+asdhfiow@users.noreply.github.com>
2025-09-13 16:24:08 +0900

    Update PhoneNumberForm.java

--- 7-22/src/main/java/jp/educure/problem2/PhoneNumberForm.java
+++ 7-22/src/main/java/jp/educure/problem2/PhoneNumberForm.java
@@ -1,5 +1,18 @@
 package jp.educure.problem2;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class PhoneNumberForm {
-    
+
+    @NotBlank(message = "電話番号は必須入力です")
+    @ValidPhoneNumber
+    private String phoneNumber;
+
+    // getter & setter
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
